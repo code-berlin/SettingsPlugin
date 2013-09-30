b1d759664e4dc5095b7b6f610ddf8278f7a9fa3e
elodingens <m.tuch@code-b.com>
2013-09-30 14:56:20 +0200

    fixed imports

--- src/android/settings.java
+++ src/android/settings.java
@@ -7,9 +7,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.content.Intent;
-
 
 /**
  * This class echoes a string called from JavaScript.
