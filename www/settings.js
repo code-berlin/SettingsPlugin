a58c43d23e03ffb07109a0d1ee8cfb57f4e550b6
elodingens <m.tuch@code-b.com>
2013-10-01 11:47:54 +0200

    fixed syntax error

--- www/settings.js
+++ www/settings.js
@@ -1,4 +1,4 @@
-window.settings = function(str, callback) {;
+window.settings = function(str, callback) {
 	cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "Echo", "echo", [str]);
