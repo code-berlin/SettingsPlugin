3a0511b08c6d4c747aee1291250f26f162d13d47
elodingens <m.tuch@code-b.com>
2013-10-01 11:19:19 +0200

    changed var name

--- www/settings.js
+++ www/settings.js
@@ -1,5 +1,9 @@
- window.settings = function(str, callback) {
-        cordova.exec(callback, function(err) {
-            callback('Nothing to echo.');
-        }, "Echo", "echo", [str]);
-    };
+window.settings = function(str, callback) {;
+	cordova.exec(callback, function(err) {
+        callback('Nothing to echo.');
+    }, "Echo", "echo", [str]);
+};
+module.exports = settings;
+
+
+
